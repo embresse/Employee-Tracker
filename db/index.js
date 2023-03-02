296a1fb9bc6639a412f92c9309cd5f6a7d6aa5c3
embresse <115603992+embresse@users.noreply.github.com>
2023-03-02 18:15:22 -0500

    updated addEmployee

--- db/index.js
+++ db/index.js
@@ -1,33 +1,44 @@
 const connection = require("./connection");
 
 class DBconnection {
-    constructor (connection) {
-    this.connection = connection;}
+  constructor(connection) {
+    this.connection = connection;
+  }
 
-    findDepartments(){
-        return this.connection.promise().query(
-            "SELECT department.id, department.name FROM department;"
-        );
-    }
+  findDepartments() {
+    return this.connection
+      .promise()
+      .query("SELECT department.id, department.name FROM department;");
+  }
 
-    findRoles(){
-        return this.connection.promise().query(
-            "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
-        );
-    }
+  findRoles() {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+      );
+  }
 
-    findEmployees(){
-        return this.connection.promise().query(
-            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
-        );
-    }
-
-
-
-
+  findEmployees() {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+      );
+  }
+  createDepartment(department) {
+    return this.connection
+      .promise()
+      .query("INSERT INTO department SET ?", department);
+  }
+  createRole(role) {
+    return this.connection.promise().query("INSERT INTO role SET ?", role);
+  }
+  createEmployee(employee) {
+    return this.connection
+      .promise()
+      .query("INSERT INTO employee SET ?", employee);
+  }
 }
 
-
-
-module.exports = new DBconnection (connection);
-
+module.exports = new DBconnection(connection);
--- index.js
+++ index.js
@@ -5,126 +5,194 @@ const mysql = require("mysql2");
 const db = require("./db");
 const table = require("console.table");
 
-function init (){
-    const message = "Employee Manager";
-    figlet(message, (err, data)=> {
-        console.log(gradient.summer.multiline(data))
-        mainMenu();
-    })
+function init() {
+  const message = "Employee Manager";
+  figlet(message, (err, data) => {
+    console.log(gradient.summer.multiline(data));
+    mainMenu();
+  });
 }
-init ();
-
+init();
 
 function mainMenu() {
   prompt([
-    {type: "list",
-    message: "What would you like to do?",
-    name: "choice",
-    choices: [
-      {
-        name: "View all employees",
-        value: "VIEW_ALL_EMPLOYEES",
-      },
-      {
-        name: "View all departments",
-        value: "VIEW_ALL_DEPARTMENTS",
-      },
-      {
-        name: "Add Employee",
-        value: "ADD_EMPLOYEE",
-      },
-      {
-        name: "Update employee role",
-        value: "UPDATE_EMPLOYEE_ROLE",
-      },
-      {
-        name: "View all roles",
-        value: "VIEW_ALL_ROLES",
-      },
-      {
-        name: "Add department",
-        value: "ADD_DEPARTMENT",
-      },
-      {
-        name: "Add role",
-        value: "ADD_ROLE",
-      },
-      {
-        name: "Quit",
-        value: "QUIT",
-      },
-    ],
-  },
-]).then((res) => {
-  let choice = res.choice;
-  switch (choice) {
-    case "VIEW_ALL_EMPLOYEES":
-      viewEmployees();
-      break;
-    case "VIEW_ALL_DEPARTMENTS":
-      viewDepartment();
-      break;
-    case "ADD_EMPLOYEE":
-      addEmployee();
-      break;
-    case "UPDATE_EMPLOYEE_ROLE":
-      updateEmployeeRole();
-      break;
-    case "VIEW_ALL_ROLES":
-      viewRole();
-      break;
-    case "ADD_ROLE":
-      addRole();
-      break;
-    case "ADD_DEPARTMENT":
-      addDepartment();
-      break;
-    case "QUIT":
-      quit();
-      break;
-  }
-});
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        {
+          name: "View all employees",
+          value: "VIEW_ALL_EMPLOYEES",
+        },
+        {
+          name: "View all departments",
+          value: "VIEW_ALL_DEPARTMENTS",
+        },
+        {
+          name: "Add Employee",
+          value: "ADD_EMPLOYEE",
+        },
+        {
+          name: "Update employee role",
+          value: "UPDATE_EMPLOYEE_ROLE",
+        },
+        {
+          name: "View all roles",
+          value: "VIEW_ALL_ROLES",
+        },
+        {
+          name: "Add department",
+          value: "ADD_DEPARTMENT",
+        },
+        {
+          name: "Add role",
+          value: "ADD_ROLE",
+        },
+        {
+          name: "Quit",
+          value: "QUIT",
+        },
+      ],
+    },
+  ]).then((res) => {
+    let choice = res.choice;
+    switch (choice) {
+      case "VIEW_ALL_EMPLOYEES":
+        viewEmployees();
+        break;
+      case "VIEW_ALL_DEPARTMENTS":
+        viewDepartment();
+        break;
+      case "ADD_EMPLOYEE":
+        addEmployee();
+        break;
+      case "UPDATE_EMPLOYEE_ROLE":
+        updateEmployeeRole();
+        break;
+      case "VIEW_ALL_ROLES":
+        viewRole();
+        break;
+      case "ADD_ROLE":
+        addRole();
+        break;
+      case "ADD_DEPARTMENT":
+        addDepartment();
+        break;
+      case "QUIT":
+        quit();
+        break;
+    }
+  });
 }
 
-
 async function viewDepartment() {
-  var Depts = await db.findDepartments()
-  console.log(Depts)
-  
+  var [Depts] = await db.findDepartments();
+  console.table(Depts);
 
-
+  mainMenu();
 }
 
 async function viewRole() {
-  var Roles = await db.findRoles()
-  console.log(Roles)
+  var [Roles] = await db.findRoles();
+  console.table(Roles);
+
+  mainMenu();
 }
 
-function viewEmployees() {
-  db.findEmployees()
+async function viewEmployees() {
+ var [Employees] = await db.findEmployees();
+ console.table(Employees)
+
+ mainMenu();
 
 }
 
 function addDepartment() {
-  
+  prompt([
+    {
+      name: "name",
+      message: "What is the name of the department you would like to add?"
+    }
+  ])
+    .then(res => {
+      let name = res;
+      db.createDepartment(name)
+        .then(() => console.log(`Added ${name.name} to the database`))
+        .then(() => mainMenu())
+    })
 }
 
 function addRole() {
-  db.findDepartments()
+  db.findDepartments();
 }
 
-function addEmployee() {
+async function addEmployee() {
+  const [roleChoices] = await db.findRoles()
 
+  prompt([
+    {
+      name: "first_name",
+      message: "What is the employee's first name?"
+    },
+    {
+      name: "last_name",
+      message: "What is the employee's last name?"
+    },
+    {
+      type: "list",
+      name:"role_id",
+      message: "What is the role of the employee?",
+      choices: roleChoices.map((role)=>{
+        return {
+          name: role.title,
+          value: role.id
+        }
+      })
+    }
+  ])
+  
+  .then (res => {
+    let roleId = res.role_id;
+    db.findEmployees()
+      .then(([data])=> {
+        let employees = data;
+        const managerSelect = employees.map(({id, first_name, last_name}) => ({
+          name: `${first_name} ${last_name}`,
+          value: id
+        }));
 
+        prompt ({
+          type: "list",
+          name: "manager_id",
+          message: "Who's the employee's manager?",
+          choices: managerSelect
+        })
+          .then(res => {
+            let employee = {
+              manager_id: res.manager_id,
+              role_id: roleId,
+              first_name: res.first_name,
+              last_name: res.last_name
+            }
+            db.createEmployee(employee)
+            .then(()=> console.log(employee))
+            .then(() => console.log(`Added ${firstName} ${lastName} to the database`))
+            .then(() => mainMenu())
 
-  db.findRoles()
+          })
+      })
+   
+  } )
+
 }
 
 function updateEmployeeRole() {
-  db.findEmployees()
+  
+  db.findEmployees();
 }
 
 function quit() {
-    console.log("Goodbye!")
-    return process.exit();
+  console.log("Goodbye!");
+  return process.exit();
 }
